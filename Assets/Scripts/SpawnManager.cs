4f48761decac1fe44371ea84745378e9a1f79136
Valsept <70474120+Valsept@users.noreply.github.com>
2022-02-20 00:44:15 +0100

    update: control zombie

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,8 +8,10 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] zombiePreFab;
     public float spawnTime = 1.0f;
     public float spwanDelay = 0.3f;
+
     public static bool spawnAllowed;
     public int ennemyCount, randZombie, randSpawnPoint;
+    Vector2 spawnPoint;
 
     // Start is called before the first frame update
     void Start()
@@ -20,12 +22,14 @@ public class SpawnManager : MonoBehaviour
     }
 
 
-    void SpwanNewEnemy(){
-        SpawnPoints = SpawnPoints
+    void SpwanNewEnemy(){   
         if (spawnAllowed){
             randZombie = Random.Range(0, zombiePreFab.Length);
             randSpawnPoint = Random.Range(0, spawnPoints.Length);
+            float randomTime = randomTime.Range(spwanDelay, spawnTime);
             Instantiate(zombiePreFab[randZombie], spawnPoints[randSpawnPoint].position, Quaternion.identity);
         }     
     }
+
+
 }
