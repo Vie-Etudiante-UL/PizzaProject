4639f84a493b388a3eb1ce26a6caafcf8092d6fa
AntoineKibok <antoinespam@protonmail.com>
2022-02-19 14:54:12 +0100

    [Feature] Furnance start

---
+++ Assets/Scripts/Furnance.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Furnance : MonoBehaviour
+{
+
+    public int pizzasWaiting, pizzaReady;
+    public float timer;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
