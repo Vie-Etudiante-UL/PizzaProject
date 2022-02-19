d0fd4ee12e9e678bd1d5a19200b03d7d01028120
Kikkawa <sylve.renault@yahoo.fr>
2022-02-19 12:25:23 +0100

    [FEATURE]PIZZA LAUNCHING

---
+++ Assets/Scripts/PizzaLauncher.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PizzaLauncher : MonoBehaviour
+{
+    public int pizzaMunitions = 10;
+    [SerializeField] float speed = 1.2f;
+    [SerializeField] GameObject pizza;
+    [SerializeField] GameObject launcher;
+    public static PizzaLauncher instance;
+    Vector2 mouseOnScreen;
+    Vector2 positionOnScreen;
+    GameObject p;
+    //[SerializeField] Material mat;
+    [SerializeField] int minValue = -90;
+    [SerializeField] int maxValue = 90;
+    void Awake()
+    {
+        if (instance != null)
+            Debug.LogWarning("Multiple instance of same Singleton : PizzaLauncher");
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+/*        if(pizzaMunitions != 10)
+        {
+            pizzaMunitions = 10;
+        }*/
+    }
+    void FixedUpdate()
+    {
+        //Get the Screen positions of the object
+        positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+        //Get the Screen position of the mouse
+        mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+        angle = Mathf.Clamp(angle, minValue, maxValue);
+        launcher.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle + 90f));
+        //Debug.DrawRay(transform.position, transform.TransformDirection(new Vector3(0, 1.0f, 0.0f)) * 1000.0f, Color.yellow);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("hello world");
+            p = Instantiate(pizza, launcher.transform.position, transform.rotation);
+
+            p.GetComponent<Rigidbody2D>().velocity = launcher.transform.up * speed;
+            Debug.Log(p.transform.forward * speed);
+        }
+
+    }
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
+}
---
+++ Assets/Scripts/RayCastRotation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastRotation : MonoBehaviour
+{
+
+
+
+
+
+
+    // Update is called once per frame
+
+    private void Update()
+    {
+        
+    }
+
+}
