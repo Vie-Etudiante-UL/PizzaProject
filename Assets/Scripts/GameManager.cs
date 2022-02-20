f7ec1287ff7ea2874afe5ce7a3058dbbc0f8069f
Kikkawa <sylve.renault@yahoo.fr>
2022-02-20 17:05:02 +0100

    new Game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,14 @@ public class GameManager : MonoBehaviour
     public Texture2D cursorTexture;
     public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotSpot = Vector2.zero;
+    public bool isPaused = false;
+    public static GameManager instance;
+    void Awake()
+    {
+        if (instance != null)
+            Debug.LogWarning("Multiple instance of same Singleton : GameManager");
+        instance = this;
+    }
     void Start()
     {
         Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
